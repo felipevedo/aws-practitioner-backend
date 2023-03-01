738df7c2d14c7fc40e8b5ebbc52e9eefe4d9061c
Andres Acevedo <andres_acevedo@epam.com>
2023-03-01 12:29:41 -0500

    setup services and add cors

---
+++ product-service/handler.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { products } = require('./mock-products.js');
+
+module.exports.getProductsList = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(
+      products,
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.getProductsById = async (event) => {
+  const { productId } = event.pathParameters;
+  const foundProduct = products.find(p => p.id === productId) || {};
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(
+      foundProduct,
+      null,
+      2
+    ),
+  };
+};
