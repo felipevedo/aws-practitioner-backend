2306a521a4091a0c3c9069eb220bd2c611a9d943
Andres Acevedo <andres_acevedo@epam.com>
2023-03-15 17:31:55 -0500

    task 5.2: create importProductsFile lambda and set permissions

--- import-service/handler.js
+++ import-service/handler.js
@@ -1,15 +1,52 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+AWS.config.update({ accessKeyId: process.env.ACCESS_ID, secretAccessKey: process.env.SECRET });
+
+const s3 = new AWS.S3({ region: 'us-east-1' });
+const BUCKET = 'import-service-store';
+
+
 module.exports.importProductsFile = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+  if (!event.queryStringParameters.name) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
       },
-      null,
-      2
-    ),
+      body: JSON.stringify({ error: 'no valid name was provided' }),
+    }
+  }
+
+  const fileName = event.queryStringParameters.name || '-';
+  const uploadsPath = `uploaded/${fileName}`;
+  const params = {
+    Bucket: BUCKET,
+    Key: uploadsPath,
+    Expires: 60,
+    ContentType: 'text/csv'
   };
+
+  try {
+    const url = s3.getSignedUrl('putObject', params);
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: url,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({ error }),
+    }
+  }
 };
